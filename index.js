e07ed374921fa6ba38594cc4e0bbbaa8b4a93126
nhutisd <nhut.px@isdcorp.vn>
2024-02-02 17:12:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+
+//Tạo server với express
+var app = express();
+app.get("/", (req, res) => {
+  res.send("Hello, world!!!!");
+});
+// Create the endpoint for your webhook
+app.post("/webhook", (req, res) => {
+  let body = req.body;
+
+  console.log(`\u{1F7EA} Received webhook:`);
+  console.dir(body, { depth: null });
+
+  // Send a 200 OK response if this is a page webhook
+
+  if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+
+    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+// Add support for GET requests to our webhook
+app.get("/messaging-webhook", (req, res) => {
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === config.verifyToken) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+// Khởi tạo server tại port 4000
+app.listen(5000);
+console.log("Running server at http://localhost:5000");
